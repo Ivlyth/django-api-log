ed16aab474ec5ef5b79cd507a7d082c223d1c69c
Myth <belongmyth@163.com>
2017-04-25 17:29:15 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='django_api_log',
     version='0.1.0',
     packages=['django_api_log', 'django_api_log.migrations'],
-    url='http://django_api_log.py.myth.ren',
+    url='https://github.com/MythRen/django-api-log',
     license='BSD',
     author='Myth',
-    author_email='email4myth@gmail.com',
+    author_email='belongmyth@163.com',
     description=''
 )
