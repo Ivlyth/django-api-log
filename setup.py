1e03a131fa630e80a2a025232c031536ee8491b9
Myth <belongmyth@163.com>
2017-04-25 17:34:44 +0800

    update description

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='django_api_log',
-    version='0.1.0',
+    version='0.1.1',
     packages=['django_api_log', 'django_api_log.migrations'],
     url='https://github.com/MythRen/django-api-log',
     license='BSD',
     author='Myth',
     author_email='belongmyth@163.com',
-    description=''
+    description='Django reusable app for record request/response information to database for alert and audit.'
 )
